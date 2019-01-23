882d57e4492459f62136e2b1a2743c448f9e7e75
Marcin Karmelita <marcin@apptailors.co>
2019-01-23 21:13:35 +0100

    Feature/update docs (#9)
    * Add Swift version
    * Update docs

--- Example-iOS-ObjC/AppDelegate.m
+++ Example-iOS-ObjC/AppDelegate.m
@@ -9,8 +9,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     BacktraceCredentials *credentials = [[BacktraceCredentials alloc]
-                                         initWithEndpoint: [NSURL URLWithString: @"https://yolo.sp.backtrace.io:6098"]
-                                         token: @"b06c6083414bf7b8e200ad994c9c8ea5d6c8fa747b6608f821278c48a4d408c3"];
+                                         initWithEndpoint: [NSURL URLWithString: @""]
+                                         token: @""];
     [BacktraceClient.shared registerWithCredentials: credentials];
 
     return YES;
