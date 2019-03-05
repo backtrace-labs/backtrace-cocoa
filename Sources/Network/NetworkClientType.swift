3a9e9ba728025caca5b32aa042356c35e4e872b1
Marcin Karmelita <marcin@apptailors.co>
2019-03-05 20:18:33 +0100

    Fix Cocoapods builds

--- Sources/Network/NetworkClientType.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-protocol NetworkClientType {
-    var successfulSendTimestamps: [TimeInterval] { get set }
-    func send(_ report: BacktraceCrashReport) throws -> BacktraceResult
-    var delegate: BacktraceClientDelegate? { get set }
-}
