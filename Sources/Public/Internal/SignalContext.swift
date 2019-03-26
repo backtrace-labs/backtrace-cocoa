1245537888c50fbc4204d7eb387117a9dc95482c
Marcin Karmelita <marcin@apptailors.co>
2019-03-26 21:09:54 +0100

    Feature/attributes (#22)
    * Improve code coverage
    * Check if the debugger is attached
    * Run CrashReporter only when the debugger is detached (by default)
    * Add reporting policy
    * Update BacktraceClientConfiguration
    * Update README.md
    * Improve documention
    * Add additional attributes for NFC and location
    * Add default attributes for: 
    - `guid` - unique user identifier
    - `error.message` 
    - `hostaname`
    - `lang.name`
    - `lang.version`
    - NFC
    - location

--- Sources/Public/Internal/SignalContext.swift
+++ Sources/Public/Internal/SignalContext.swift
@@ -4,4 +4,5 @@ protocol SignalContext: CustomStringConvertible {
     var attributes: Attributes { get }
     var userAttributes: Attributes { get set }
     var defaultAttributes: Attributes { get }
+    func set(faultMessage: String?)
 }
