3b8c835230c03dfa45f474dfa78d16916367e750
Rafał Kitta <rafalkit@gmail.com>
2019-04-24 22:10:33 +0200

    Add crashing action to demo app

--- Example-tvOS/ViewController.swift
+++ Example-tvOS/ViewController.swift
@@ -7,6 +7,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func crashButtonTapped(_ sender: Any) {
+        let items = [String]()
+        _ = items[1]
+    }
 }
 
