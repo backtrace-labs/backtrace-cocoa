fa6196ffa51463aa3c1c1c16cd6ab742b325a286
Marcin Karmelita <marcin@apptailors.co>
2019-05-18 13:27:38 +0200

    Update unit tests

--- Tests/AttributesTests.swift
+++ Tests/AttributesTests.swift
@@ -7,31 +7,29 @@ import Quick
 final class AttributesTests: QuickSpec {
     
     override func spec() {
-        describe("Memory") {
-            throwingContext("Exclude from context", closure: {
-                it("non-existing file", closure: {
-                    expect { try Memory.vmStatistics64() }.toNot(be(vm_statistics64()))
-                })
-                
-                it("non-existing file", closure: {
-                    expect { try Processor.processorSetLoadInfo() }.toNot(be(processor_set_load_info()))
-                })
-                
-                it("non-existing file", closure: {
-                    expect { try Processor.processorCpuLoadInfo() }.toNot(beEmpty())
-                })
-                
-                it("non-existing file", closure: {
-                    expect { try Processor.machTaskBasicInfo() }.toNot(be(mach_task_basic_info()))
-                })
-                
-                it("non-existing file", closure: {
-                    expect { try Processor.taskVmInfo() }.toNot(be(task_vm_info()))
-                })
-                
-                it("non-existing file", closure: {
-                    expect { try Processor.taskEventsInfo() }.toNot(be(task_events_info()))
-                })
+        describe("C API") {
+            it("provides vm_statistics64 information", closure: {
+                expect { try Memory.vmStatistics64() }.toNot(be(vm_statistics64()))
+            })
+            
+            it("provides processor_set_load_info information", closure: {
+                expect { try Processor.processorSetLoadInfo() }.toNot(be(processor_set_load_info()))
+            })
+            
+            it("provides [processor_cpu_load_info] information", closure: {
+            expect { try Processor.processorCpuLoadInfo() }.toNot(beEmpty())
+            })
+            
+            it("provides mach_task_basic_info information", closure: {
+                expect { try Processor.machTaskBasicInfo() }.toNot(be(mach_task_basic_info()))
+            })
+            
+            it("provides task_vm_info information", closure: {
+                expect { try Processor.taskVmInfo() }.toNot(be(task_vm_info()))
+            })
+            
+            it("provides task_events_info information", closure: {
+                expect { try Processor.taskEventsInfo() }.toNot(be(task_events_info()))
             })
         }
     }
