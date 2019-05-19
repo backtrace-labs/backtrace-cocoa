8ddb18c7578ef346b69dd730d0f7e05098603b94
Marcin Karmelita <marcin@apptailors.co>
2019-05-19 17:42:37 +0200

    Memory system and virtual memory information

--- Sources/Features/Attributes/DefaultAttributes.swift
+++ Sources/Features/Attributes/DefaultAttributes.swift
@@ -22,44 +22,44 @@ protocol AttributesSourceType {
 
 struct ProcessorInfo: AttributesSourceType {
     
-    private enum Key: String {
-        case cpuContext = "cpu.context"
-        case cpuIdle = "cpu.idle"
-        case cpuIowait = "cpu.iowait"
-        case cpuIrq = "cpu.irq"
-        case cpuKernel = "cpu.kernel"
-        case cpuNice = "cpu.nice"
-        case cpuProcessBlocked = "cpu.process.blocked"
-        case cpuProcessCount = "cpu.process.count"
-        case cpuProcessRunning = "cpu.process.running"
-        case cpuSoftirq = "cpu.softirq"
-        case cpuUser = "cpu.user"
-        case cpuSystem = "cpu.system"
-        case descriptorCount = "descriptor.count"
-        case processThreadCount = "process.thread.count"
-        case systemBoottime = "cpu.boottime"
-        case processorCount = "cpu.count"
-        case processorActive = "cpu.active"
-        case hostname = "hostname"
-    }
-    
     static func current() -> Attributes {
         let processor = try? Processor()
         let processinfo = ProcessInfo.processInfo
+        let memory = try? Memory()
+        let swapMemory = try? Memory.Swap()
         
         let keyValuePairs: [String: Any?] = [
-            Key.cpuIdle.rawValue: processor?.cpuTicks.idle,
-            Key.cpuNice.rawValue: processor?.cpuTicks.nice,
-            Key.cpuUser.rawValue: processor?.cpuTicks.user,
-            Key.cpuSystem.rawValue: processor?.cpuTicks.system,
-            Key.descriptorCount.rawValue: getdtablesize(),
-            Key.cpuProcessCount.rawValue: processor?.processorSetLoadInfo.task_count,
-            Key.processThreadCount.rawValue: processor?.processorSetLoadInfo.thread_count,
-            Key.hostname.rawValue: processinfo.hostName,
-            Key.systemBoottime.rawValue: try? System.boottime(),
-            Key.processorCount.rawValue: processinfo.processorCount,
-            Key.processorActive.rawValue: processinfo.activeProcessorCount,
-            Key.cpuContext.rawValue: processor?.taskEventsInfo.csw]
+            // hostanme
+            "hostname": processinfo.hostName,
+            // descriptor
+            "descriptor.count": getdtablesize(),
+            // cpu
+            "cpu.idle": processor?.cpuTicks.idle,
+            "cpu.nice": processor?.cpuTicks.nice,
+            "cpu.user": processor?.cpuTicks.user,
+            "cpu.system": processor?.cpuTicks.system,
+            "cpu.process.count": processor?.processorSetLoadInfo.task_count,
+            "cpu.boottime": try? System.boottime(),
+            "cpu.count": processinfo.processorCount,
+            "cpu.count.active": processinfo.activeProcessorCount,
+            "cpu.context": processor?.taskEventsInfo.csw,
+            // process
+            "process.thread.count": processor?.processorSetLoadInfo.thread_count,
+            // system
+            "system.memory.active": memory?.active,
+            "system.memory.inactive": memory?.inactive,
+            "system.memory.free": memory?.free,
+            "system.memory.used": memory?.used,
+            "system.memory.total": memory?.total,
+            "system.memory.swap.total": swapMemory?.total,
+            "system.memory.swap.used": swapMemory?.used,
+            "system.memory.swap.free": swapMemory?.free,
+            // vm
+            "vm.rss.size": memory?.resident,
+            "vm.rss.peak": memory?.residentPeak,
+            "vm.vma.size": memory?.virtual,
+        ]
+        
         
         return keyValuePairs.compactMapValues { $0 }
     }
--- Sources/Features/Attributes/Memory.swift
+++ Sources/Features/Attributes/Memory.swift
@@ -1,6 +1,31 @@
 import Foundation
 
 struct Memory {
+    let active: UInt
+    let free: UInt
+    let inactive: UInt
+    let resident: UInt
+    let residentPeak: UInt
+    let virtual: UInt
+    let used: UInt
+    let total: UInt
+    
+    init() throws {
+        let vmStatistics64 = try Memory.vmStatistics64()
+        let taskVmInfo = try Processor.taskVmInfo()
+        let pageSize = UInt(getpagesize())
+        let taskInfo = try Processor.machTaskBasicInfo()
+        self.active = UInt(vmStatistics64.active_count) * pageSize
+        self.free = UInt(vmStatistics64.free_count) * pageSize
+        self.inactive = UInt(vmStatistics64.inactive_count) * pageSize
+        self.resident = UInt(taskVmInfo.resident_size)
+        self.residentPeak = UInt(taskVmInfo.resident_size_peak)
+        self.virtual = UInt(taskVmInfo.virtual_size)
+        let wire = UInt(vmStatistics64.wire_count) * pageSize
+        self.used = self.active + self.inactive + wire
+        self.total = self.used + self.free
+    }
+    
     static func vmStatistics64() throws -> vm_statistics64 {
         var vmStatInfo = vm_statistics64()
         var size: mach_msg_type_number_t =
@@ -15,5 +40,20 @@ struct Memory {
             throw KernError.code(kernReturn)
         }
         return vmStatInfo
+    }
+    
+    struct Swap {
+        let total: UInt
+        let used: UInt
+        let free: UInt
+        
+        init() throws {
+            var usage = xsw_usage()
+            try System.systemControl(mib: [CTL_VM, VM_SWAPUSAGE], returnType: &usage)
+            
+            self.total = UInt(usage.xsu_total)
+            self.free = UInt(usage.xsu_avail)
+            self.used = UInt(usage.xsu_used)
+        }
     }
 }
