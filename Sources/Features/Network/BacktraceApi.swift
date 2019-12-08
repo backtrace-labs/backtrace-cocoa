1d1e9d3054a5d729e37a961d0ffe4a6545f2730f
Marcin Karmelita <marcin@apptailors.co>
2019-12-08 23:07:34 +0100

    Fix typos

--- Sources/Features/Network/BacktraceApi.swift
+++ Sources/Features/Network/BacktraceApi.swift
@@ -6,6 +6,7 @@ class BacktraceApi {
     var successfulSendTimestamps: [TimeInterval] = []
     var reportsPerMin: Int
     weak var delegate: BacktraceClientDelegate?
+    private let cacheInterval = 60.0
     
     init(endpoint: URL, token: String, session: URLSession = URLSession(configuration: .ephemeral),
          reportsPerMin: Int) {
@@ -19,23 +20,23 @@ extension BacktraceApi: BacktraceApiProtocol {
     func send(_ report: BacktraceReport) throws -> BacktraceResult {
         // check if can send
         let currentTimestamp = Date().timeIntervalSince1970
-        let numberOfSendsInLastOneMinute = successfulSendTimestamps.filter { currentTimestamp - $0 < 60.0 }.count
+        let numberOfSendsInLastOneMinute = successfulSendTimestamps.filter { currentTimestamp - $0 < cacheInterval }.count
         guard numberOfSendsInLastOneMinute < reportsPerMin else {
             return BacktraceResult(.limitReached, report: report)
         }
         // modify before sending
-        let modifiedBeforeSendingReport = self.delegate?.willSend?(report) ?? report
+        let modifiedBeforeSendingReport = delegate?.willSend?(report) ?? report
         let attachments = modifiedBeforeSendingReport.attachmentPaths.compactMap(Attachment.init(filePath:))
         // create request
-        let urlRequest = try self.request.multipartUrlRequest(data: modifiedBeforeSendingReport.reportData,
-                                                              attributes: modifiedBeforeSendingReport.attributes,
-                                                              attachments: attachments)
+        let urlRequest = try request.multipartUrlRequest(data: modifiedBeforeSendingReport.reportData,
+                                                         attributes: modifiedBeforeSendingReport.attributes,
+                                                         attachments: attachments)
         BacktraceLogger.debug("Sending crash report:\n\(urlRequest.debugDescription)")
         // send report
         let response = session.sync(urlRequest)
         // check network error
-        if let responseError = response.reponseError {
-            self.delegate?.connectionDidFail?(responseError)
+        if let responseError = response.responseError {
+            delegate?.connectionDidFail?(responseError)
             throw HttpError.connectionError(responseError)
         }
         guard let httpResponse = response.urlResponse, let responseData = response.responseData else {
@@ -47,13 +48,13 @@ extension BacktraceApi: BacktraceApiProtocol {
         switch result {
         case .error(let error):
             BacktraceLogger.debug("Response: \n\(error)")
-            self.delegate?.serverDidResponse?(error.result(report: modifiedBeforeSendingReport))
+            delegate?.serverDidResponse?(error.result(report: modifiedBeforeSendingReport))
             return error.result(report: modifiedBeforeSendingReport)
         case .success(let response):
             // did send successfully
             BacktraceLogger.debug("Response: \n\(response)")
-            self.successfulSendTimestamps.append(Date().timeIntervalSince1970)
-            self.delegate?.serverDidResponse?(response.result(report: modifiedBeforeSendingReport))
+            successfulSendTimestamps.append(Date().timeIntervalSince1970)
+            delegate?.serverDidResponse?(response.result(report: modifiedBeforeSendingReport))
             return response.result(report: modifiedBeforeSendingReport)
         }
     }
