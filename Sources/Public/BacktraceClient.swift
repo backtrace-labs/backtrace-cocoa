1976097c0380052eee094c8045e054bb3f41880c
konraddysput <konrad.dysput@gmail.com>
2021-02-10 15:25:07 +0100

    Removed if brackets

--- Sources/Public/BacktraceClient.swift
+++ Sources/Public/BacktraceClient.swift
@@ -153,7 +153,7 @@ extension BacktraceClient: BacktraceReporting {
             return
         }
         
-        if(self.configuration.detectOOM) {
+        if self.configuration.detectOOM {
             self.reporter.enableOomWatcher()
         }
         
