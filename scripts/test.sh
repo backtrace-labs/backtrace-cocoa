a9667908b9ae85f050be50394b70154124269d71
Marcin Karmelita <marcin@apptailors.co>
2021-02-13 23:49:46 +0100

    Set up GitHub actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -ex
+set -o errexit # make your script exit when a command fails
+set -o pipefail # to exit when the status of the last command that threw a non-zero exit code is returned
+set -o nounset # to exit when your script tries to use undeclared variables
+set -o xtrace # to trace what gets executed. Useful for debugging
 
 fastlane ios tests
 fastlane mac tests
