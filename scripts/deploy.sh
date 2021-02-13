dd6fd3d6009a86046965e86f964951fee92dde06
Marcin Karmelita <marcin@apptailors.co>
2021-02-13 22:40:06 +0100

    Add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+set -o errexit # make your script exit when a command fails
+set -o pipefail # to exit when the status of the last command that threw a non-zero exit code is returned
+set -o nounset # to exit when your script tries to use undeclared variables
+set -o xtrace # to trace what gets executed. Useful for debugging
+
+# This script requires the `COCOAPODS_TRUNK_TOKEN` env var to be set. 
+# See more: https://fuller.li/posts/automated-cocoapods-releases-with-ci/.
+
+pod trunk push Backtrace.podspec --allow-warnings
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,3 +4,5 @@ set -ex
 fastlane ios tests
 fastlane mac tests
 fastlane tvos tests
+
+pod lib lint --verbose --allow-warnings --sources='https://cdn.cocoapods.org/'
