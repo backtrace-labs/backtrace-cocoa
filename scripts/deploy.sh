546cf1a63837d975c3d83ae3053031acc48a57ab
Vincent Lussenburg <vlussenburg@users.noreply.github.com>
2022-05-06 11:40:26 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,6 @@ set -o pipefail # to exit when the status of the last command that threw a non-z
 set -o nounset # to exit when your script tries to use undeclared variables
 set -o xtrace # to trace what gets executed. Useful for debugging
 
-sh scripts/test.sh
-
 # This script requires the `COCOAPODS_TRUNK_TOKEN` env var to be set. 
 # See more: https://fuller.li/posts/automated-cocoapods-releases-with-ci/.
 pod trunk push Backtrace.podspec --allow-warnings
