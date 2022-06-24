b0a391cb1a4fff3c9b710603fffdb869fd0635fa
MinPD126 <meenup@bitheads.com>
2022-06-24 23:31:47 +0530

    Update BacktraceBreadcrumbTests.swift

--- Tests/BacktraceBreadcrumbTests.swift
+++ Tests/BacktraceBreadcrumbTests.swift
@@ -12,22 +12,25 @@ final class BacktraceBreadcrumbTests: QuickSpec {
                 BacktraceCredentials(endpoint: URL(string: "https://yourteam.backtrace.io")!, token: "")
             let dbSettings = BacktraceDatabaseSettings()
             let reportsPerMin = 3
-            var configuration: BacktraceClientConfiguration!
+            var configuration: BacktraceClientConfiguration?
 
             beforeEach {
                 configuration = BacktraceClientConfiguration(credentials: credentials, dbSettings: dbSettings,
                                                                  reportsPerMin: reportsPerMin)
             }
+            afterEach {
+                configuration = nil
+            }
             context("breadcrumb is not enabled") {
                 it("fails to add breadcrumb") {
-                    let result = configuration.addBreadcrumb("Breadcrumb submit test")
+                    let result = configuration?.addBreadcrumb("Breadcrumb submit test")
                     expect { result }.to(beFalse())
                 }
             }
             context("breadcrumb is enabled") {
                 it("Able to add breadcrumb") {
-                    configuration.enableBreadCrumbs()
-                    let result = configuration.addBreadcrumb("Breadcrumb submit test")
+                    configuration?.enableBreadCrumbs()
+                    let result = configuration?.addBreadcrumb("Breadcrumb submit test")
                     expect { result }.to(beTrue())
                 }
             }
