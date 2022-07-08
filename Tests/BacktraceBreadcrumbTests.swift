2af96a7b1612688cc74015f62333e59b4e07d3fd
MinPD126 <meenup@bitheads.com>
2022-07-09 01:33:21 +0530

    Test breadcrumb text

--- Tests/BacktraceBreadcrumbTests.swift
+++ Tests/BacktraceBreadcrumbTests.swift
@@ -6,12 +6,38 @@ import Quick
 
 final class BacktraceBreadcrumbTests: QuickSpec {
     
+    let breadcrumbLogFileName = "bt-breadcrumbs-0"
+    let defaultMaxLogSize = 64000
+    func breadcrumbLogPath(_ create: Bool) throws -> String {
+        var fileURL = try FileManager.default.url(for: .documentDirectory,
+                                                  in: .userDomainMask,
+                                                  appropriateFor: nil,
+                                                  create: create)
+        fileURL.appendPathComponent(breadcrumbLogFileName)
+        return fileURL.path
+    }
+    
+    func readBreadcrumbText() -> String? {
+        do {
+            let path = try breadcrumbLogPath(true)
+            let fileURL = URL(fileURLWithPath: path)
+            let content = try String(contentsOf: fileURL, encoding: .ascii)
+            return content
+        } catch {
+            return nil
+        }
+    }
+    
     override func spec() {
         describe("Breadcrumbs") {
             
             var breadCrumb: BacktraceBreadcrumb?
 
             beforeEach {
+                do {
+                    try FileManager.default.removeItem(atPath: self.breadcrumbLogPath(false))
+                } catch {
+                }
                 breadCrumb = BacktraceBreadcrumb()
             }
             afterEach {
@@ -23,6 +49,8 @@ final class BacktraceBreadcrumbTests: QuickSpec {
                     expect { breadCrumb?.getCurrentBreadcrumbId }.to(beNil())
                     let result = breadCrumb?.addBreadcrumb("Breadcrumb submit test")
                     expect { result }.to(beFalse())
+                    let breadcrumbText = self.readBreadcrumbText()
+                    expect { breadcrumbText?.contains("Breadcrumb submit test") }.to(beFalse())
                 }
             }
             context("breadcrumb is enabled") {
@@ -32,6 +60,8 @@ final class BacktraceBreadcrumbTests: QuickSpec {
                     expect { breadCrumb?.getCurrentBreadcrumbId }.toNot(beNil())
                     let result = breadCrumb?.addBreadcrumb("Breadcrumb submit test")
                     expect { result }.to(beTrue())
+                    let breadcrumbText = self.readBreadcrumbText()
+                    expect { breadcrumbText?.contains("Breadcrumb submit test") }.to(beTrue())
                 }
                 it("Again disable breadcrumb") {
                     breadCrumb?.disableBreadCrumbs()
