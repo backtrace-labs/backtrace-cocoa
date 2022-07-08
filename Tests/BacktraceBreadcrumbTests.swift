740ea220f24c4d9a5cc43fdb2c1d2a882b09edab
MinPD126 <meenup@bitheads.com>
2022-07-09 00:49:08 +0530

    Revert "Update BacktraceBreadcrumbTests.swift"
    This reverts commit 8f3dcf149896df9ee098ee8f6a11318f791364a7.

--- Tests/BacktraceBreadcrumbTests.swift
+++ Tests/BacktraceBreadcrumbTests.swift
@@ -6,49 +6,12 @@ import Quick
 
 final class BacktraceBreadcrumbTests: QuickSpec {
     
-    let breadcrumbLogFileName = "bt-breadcrumbs-0"
-    let defaultMaxLogSize = 64000
-    func breadcrumbLogPath(_ create: Bool) throws -> String {
-        var fileURL = try FileManager.default.url(for: .documentDirectory,
-                                                  in: .userDomainMask,
-                                                  appropriateFor: nil,
-                                                  create: create)
-        fileURL.appendPathComponent(breadcrumbLogFileName)
-        return fileURL.path
-    }
-    
-    func raadBreadcrumb() -> [[String:Any]]? {
-        do {
-            let path = try breadcrumbLogPath(true)
-            let fileURL = URL(fileURLWithPath: path)
-            let content = try String(contentsOf: fileURL, encoding: .utf8)
-            return convertStringIntoBreadcrumb(content)
-        } catch {
-            BacktraceLogger.warning("\(error.localizedDescription) \nWhen try to read breadcrumbs log file")
-            return nil
-        }
-    }
-    
-    func convertStringIntoBreadcrumb(_ text: String) -> [[String:Any]]? {
-        if let data = text.data(using: .utf8) {
-            do {
-                return try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]]
-            } catch {
-                BacktraceLogger.warning("\(error.localizedDescription) \nWhen try to convert text into json object")
-                return nil
-            }
-        }
-        return nil
-    }
-    
     override func spec() {
         describe("Breadcrumbs") {
+            
             var breadCrumb: BacktraceBreadcrumb?
+
             beforeEach {
-                do {
-                    try FileManager.default.removeItem(atPath: try self.breadcrumbLogPath(false))
-                } catch {
-                }
                 breadCrumb = BacktraceBreadcrumb()
             }
             afterEach {
@@ -60,8 +23,6 @@ final class BacktraceBreadcrumbTests: QuickSpec {
                     expect { breadCrumb?.getCurrentBreadcrumbId }.to(beNil())
                     let result = breadCrumb?.addBreadcrumb("Breadcrumb submit test")
                     expect { result }.to(beFalse())
-                    let breadcrumbs = self.raadBreadcrumb()
-                    expect { breadcrumbs?.count ?? 0 }.to(equal(0))
                 }
             }
             context("breadcrumb is enabled") {
@@ -71,8 +32,6 @@ final class BacktraceBreadcrumbTests: QuickSpec {
                     expect { breadCrumb?.getCurrentBreadcrumbId }.toNot(beNil())
                     let result = breadCrumb?.addBreadcrumb("Breadcrumb submit test")
                     expect { result }.to(beTrue())
-                    let breadcrumbs = self.raadBreadcrumb()
-                    expect { breadcrumbs?.count ?? 0 }.to(equal(1))
                 }
                 it("Again disable breadcrumb") {
                     breadCrumb?.disableBreadCrumbs()
