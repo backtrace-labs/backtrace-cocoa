935a4386358865b117a9ae797661b1e4d25f8c37
vlussenburg <vlussenburg@backtrace.io>
2022-07-13 13:21:45 -0600

    Added unit test fixes for delay, set delay to 5 seconds instead of 10.

--- Sources/Features/Metrics/Model/BacktraceMetricsSender.swift
+++ Sources/Features/Metrics/Model/BacktraceMetricsSender.swift
@@ -30,7 +30,7 @@ final class BacktraceMetricsSender {
     func enable() {
         // TODO: this timeout is set to give the crashreporter time to start up so that errors during startup will be sent
         // remove once root cause for crashes is found
-        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 10) {
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + .seconds(5)) {
             self.sendStartupEvents()
         }
     }
