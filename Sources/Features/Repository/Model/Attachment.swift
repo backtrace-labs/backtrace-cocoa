a4352e4dcd76440f6c09646f44bbde327b214335
MinPD126 <meenup@bitheads.com>
2022-08-09 23:28:54 +0530

    Update Attachment.swift

--- Sources/Features/Repository/Model/Attachment.swift
+++ Sources/Features/Repository/Model/Attachment.swift
@@ -6,18 +6,32 @@ import CoreServices
 #endif
 
 struct Attachment {
-    let data: Data
+    let data: Data?
     let name: String
     let mimeType: String
 
+    // Make sure attachments are not bigger than 10 MB.
+    private static let maximumAttachmentSize = 10 * 1024 * 1024
+    
     init?(filePath: String) {
-        let fileURL = URL(fileURLWithPath: filePath)
-        guard let fileData = try? Data(contentsOf: fileURL,
-                                       options: Data.ReadingOptions.mappedIfSafe) else { return nil }
-
-        mimeType = Attachment.mimeTypeForPath(fileUrl: fileURL)
-        name = "attachment_" + (fileURL.lastPathComponent as NSString).deletingPathExtension + "_\(arc4random())"
-        data = fileData
+        do {
+            let fileURL = URL(fileURLWithPath: filePath)
+            
+            let fileAttributes: [FileAttributeKey : Any] = try FileManager.default.attributesOfItem(atPath: filePath)
+            
+            if let fileSize = fileAttributes[FileAttributeKey.size] as? UInt64,
+               Attachment.maximumAttachmentSize >= fileSize,
+               let fileData = try? Data(contentsOf: fileURL,
+                                              options: Data.ReadingOptions.mappedIfSafe) {
+                mimeType = Attachment.mimeTypeForPath(fileUrl: fileURL)
+                name = "attachment_" + (fileURL.lastPathComponent as NSString).deletingPathExtension + "_\(arc4random())"
+                data = fileData
+            } else {
+                return nil
+            }
+        } catch  {
+            return nil
+        }
     }
 
     static private func mimeTypeForPath(fileUrl: URL) -> String {
