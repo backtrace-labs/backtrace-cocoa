bd5c9ac4442fbec3e38e26f1104f99cf6959dc10
Konstantin R <111510241+konst-sauce@users.noreply.github.com>
2022-08-22 22:19:56 +0200

    Polished setting up Keys to configure Backtrace.
    1. Added formatting of final string, so user needs just to provide universe name and token, final endpoint URLs are formatted based on this.
    2. Updated URLs to help sources - previous ones returned "Not found error".
    3. Refreshed IBAction references for buttons in tvOS example app.

--- Examples/Common/Keys.h
+++ Examples/Common/Keys.h
@@ -4,11 +4,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Keys : NSObject
 
-/// [Submission URL](https://help.backtrace.io/en/articles/1772855-what-is-a-submission-url)
+@property(class, nonatomic, assign, readonly) NSString *universeName;
+
+/// [Submission URL](https://support.backtrace.io/hc/en-us/articles/360040516451-What-is-a-submission-url-)
 @property(class, nonatomic, assign, readonly) NSString *backtraceUrl;
 @property(class, nonatomic, assign, readonly) NSString *backtraceSubmissionUrl;
 
-/// [Submission token](https://help.backtrace.io/en/articles/1772818-what-is-a-submission-token)
+/// [Submission token](https://support.backtrace.io/hc/en-us/articles/360040105172)
 @property(class, nonatomic, assign, readonly) NSString *backtraceToken;
 
 @end
