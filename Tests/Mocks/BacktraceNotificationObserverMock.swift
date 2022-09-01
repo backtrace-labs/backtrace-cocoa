6eb2d753be62156a614cf93c3c5eb22d2c13d321
Vincent Lussenburg <vlussenburg@users.noreply.github.com>
2022-08-31 18:04:50 -0600

    Prevent duplicate breadcrumbs (#92)
    * Prevent duplicate breadcrumbs from being added
    
    Co-authored-by: MinPD126 <meenup@bitheads.com>

--- Tests/Mocks/BacktraceNotificationObserverMock.swift
+++ Tests/Mocks/BacktraceNotificationObserverMock.swift
@@ -6,6 +6,7 @@ import XCTest
 class BacktraceObserverMock: BacktraceNotificationHandlerDelegate {
 
     var delegate: BacktraceNotificationObserverDelegate?
+
     var startObservingCalled = false
 
     func startObserving(_ delegate: BacktraceNotificationObserverDelegate) {
