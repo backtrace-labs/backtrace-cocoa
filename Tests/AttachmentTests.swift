320036e846ba648ffae57d14d5896a78df48f852
vlussenburg <vlussenburg@backtrace.io>
2022-08-31 18:03:14 -0600

    Attempt to improve test reliability.

--- Tests/AttachmentTests.swift
+++ Tests/AttachmentTests.swift
@@ -36,9 +36,18 @@ final class AttachmentTests: QuickSpec {
                 }
 
                 throwingIt("attachment won't init if size is larger than 10 MB") {
-                    try NSMutableData.init(bytes: [], length: 11 * 1024 * 1024).write(toFile: "11mb.file")
-                    let attachment = Attachment(filePath: "11mb.file")
+                    let path = FileManager.default.temporaryDirectory.appendingPathComponent("11mb.file").path
+
+                    try NSMutableData.init(bytes: [], length: 11 * 1024 * 1024).write(toFile: path)
+                    expect { FileManager.default.fileExists(atPath: path)}.to(beTrue())
+
+                    // test reliability, without this test fails intermittently
+                    let fileSize = try FileManager.default.attributesOfItem(atPath: path)[.size] as? UInt64
+                    expect { fileSize }.toEventually(beGreaterThan(10 * 1024 * 1024))
+
+                    let attachment = Attachment(filePath: path)
                     expect(attachment).to(beNil())
+                    try FileManager.default.removeItem(atPath: path)
                 }
             }
         }
