16c69f979d9c09e9cb9bb68958e71b5c3af068a5
harissauce <melek.ramki@saucelabs.com>
2023-04-14 05:02:02 -0400

    Update github workflows SWIFT_VERSION

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,4 +8,4 @@ fastlane ios tests
 fastlane mac tests
 fastlane tvos tests
 
-#pod lib lint --verbose --allow-warnings --sources='https://cdn.cocoapods.org/'
+pod lib lint --verbose --allow-warnings --sources='https://cdn.cocoapods.org/'
