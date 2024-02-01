3b555b2fb9be65bb21a7fb731cdc9b57f21603a7
Konrad Dysput <konrad.dysput@gmail.com>
2024-02-01 21:31:52 +0100

    Privacy info  (#120)
    * Breadcrumb overflow
    
    * Version update
    
    * Fix a problem with invalid breadcrumb file size
    
    * Add support for remove subrange
    
    * Updated changelog
    
    * Privacy info file
    
    * Privacy info file
    
    * Update package

--- Sources/Features/Attributes/DefaultAttributes.swift
+++ Sources/Features/Attributes/DefaultAttributes.swift
@@ -207,7 +207,7 @@ struct LibInfo: AttributesSource {
     private static let applicationGuidKey = "backtrace.unique.user.identifier"
     private static let applicationLangName = "backtrace-cocoa"
 
-    var backtraceVersion = "2.0.2"
+    var backtraceVersion = "2.0.3"
     
     var immutable: [String: Any?] {
         return ["guid": LibInfo.guid(store: UserDefaultsStore.self).uuidString,
