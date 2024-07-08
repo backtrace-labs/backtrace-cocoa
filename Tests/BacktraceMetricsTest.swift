cb4f4e73df2313f37866e459e99f86470df06d39
Melek Ramki <melekramki@gmail.com>
2024-07-08 18:18:21 -0400

    Update BacktraceMetrics
    Add getSummedEvents and getUniqueEvents functions
    Update BacktraceMetricsTests

--- Tests/BacktraceMetricsTest.swift
+++ Tests/BacktraceMetricsTest.swift
@@ -10,12 +10,12 @@ final class BacktraceMetricsTests: QuickSpec {
         describe("Backtrace Metrics") {
             let urlSession = URLSessionMock()
             urlSession.response = MockOkResponse()
-            let credentials =
-                BacktraceCredentials(endpoint: URL(string: "https://yourteam.backtrace.io")!, token: "")
+            let credentials = BacktraceCredentials(endpoint: URL(string: "https://yourteam.backtrace.io")!, token: "")
             let backtraceApi = BacktraceApi(credentials: credentials, session: urlSession, reportsPerMin: 30)
 
             let summedEventName = "view-changed"
             let uniqueEventName = "guid"
+            let applicationLaunchEventName = "Application Launches"
             
             var metrics: BacktraceMetrics?
             
@@ -23,23 +23,127 @@ final class BacktraceMetricsTests: QuickSpec {
                 
                 beforeEach {
                     metrics = BacktraceMetrics(api: backtraceApi)
-                    metrics?.enable(settings: BacktraceMetricsSettings())
-                    metrics?.clearSummedEvents()
                 }
                 
-                it("clears the summed event after enabling and sending") {
-                    // Allow default events to be "sent" out
-                    expect { metrics?.count }.toEventually(equal(1))
+                it("clears the summed event after enabling") {
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    metrics?.clearSummedEvents()
+                    
+                    guard let summedEvents = metrics?.getSummedEventsPayload() else { return }
+                    
+                    guard let events = summedEvents.value(forKey: "events") as? [SummedEvent] else { return }
+                    
+                    expect { events.count }.to(equal(0))
                 }
                 
                 it("can add and store summed event") {
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    metrics?.clearSummedEvents()
+                    
                     metrics?.addSummedEvent(name: summedEventName)
-                    expect { metrics?.count }.to(equal(2))
+                    
+                    guard let summedEvents = metrics?.getSummedEventsPayload() else { return }
+                    
+                    guard let events = summedEvents.value(forKey: "events") as? [SummedEvent] else { return }
+                    
+                    let filteredEvents = events.filter { event in
+                        return event.name == summedEventName
+                    }
+                    
+                    expect {filteredEvents.count}.to(equal(1))
+                }
+                
+                it("can add and store application launch event") {
+                    
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    guard let summedEvents = metrics?.getSummedEventsPayload() else { return }
+                    
+                    guard let events = summedEvents.value(forKey: "events") as? [SummedEvent] else { return }
+                    
+                    let filteredEvents = events.filter { event in
+                        return event.name == applicationLaunchEventName
+                    }
+                    
+                    expect {filteredEvents.count}.to(equal(1))
                 }
                 
                 it("can add and store unique event") {
-                    metrics?.addUniqueEvent(name: uniqueEventName)
-                    expect { metrics?.count }.to(equal(2))
+                    
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    guard let uniqueEvents = metrics?.getUniqueEventsPayload() else { return }
+                    
+                    guard let events = uniqueEvents.value(forKey: "events") as? [UniqueEvent] else { return }
+                    
+                    let filteredEvents = events.filter { event in
+                        return event.name.contains(uniqueEventName)
+                    }
+                    
+                    expect {filteredEvents.count}.to(equal(1))
+                }
+            }
+            
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            context("Events operation") {
+                
+                beforeEach {
+                    metrics = BacktraceMetrics(api: backtraceApi)
+                }
+                
+                it("clears the summed event after enabling") {
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    metrics?.clearSummedEvents()
+                    
+                    guard let summedEvents = metrics?.getSummedEvents() else { return }
+                    
+                    expect { summedEvents.count }.to(equal(0))
+                }
+                
+                it("can add and store summed event") {
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    metrics?.clearSummedEvents()
+                    
+                    metrics?.addSummedEvent(name: summedEventName)
+                    
+                    guard let summedEvents = metrics?.getSummedEvents() as? [SummedEvent] else { return }
+                    
+                    let filteredEvents = summedEvents.filter { event in
+                        return event.name == summedEventName
+                    }
+                    
+                    expect {filteredEvents.count}.to(equal(1))
+                }
+                
+                it("can add and store application launch event") {
+                    
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    guard let summedEvents = metrics?.getSummedEvents() as? [SummedEvent] else { return }
+                    
+                    let filteredEvents = summedEvents.filter { event in
+                        return event.name == applicationLaunchEventName
+                    }
+                    
+                    expect {filteredEvents.count}.to(equal(1))
+                }
+                
+                it("can add and store unique event") {
+                    
+                    metrics?.enable(settings: BacktraceMetricsSettings())
+                    
+                    guard let uniqueEvents = metrics?.getUniqueEvents() as? [UniqueEvent] else { return }
+                    
+                    let filteredEvents = uniqueEvents.filter { event in
+                        return event.name.contains(uniqueEventName)
+                    }
+                    
+                    expect {filteredEvents.count}.to(equal(1))
                 }
             }
         }
