0a2de1dbce9430eb47457ab9e2c25563eda6d120
melekr <melekramki@gmail.com>
2024-08-14 17:16:18 -0400

    Generate report with exception

--- Tests/CrashReporterTests.swift
+++ Tests/CrashReporterTests.swift
@@ -20,10 +20,25 @@ final class CrashReporterTests: QuickSpec {
                 it("generates live report on demand") {
                     expect { try crashReporter.generateLiveReport(attributes: [:]) }.toNot(throwError())
                 }
+                
                 it("generate live report on demand 10 times") {
                     for _ in 0...10 {
                         expect { try crashReporter.generateLiveReport(attributes: [:]) }.toNot(throwError())
                     }
+                }
+                
+                it("generated live report without exception") {
+                    let reportData = try crashReporter.generateLiveReport(exception: nil, attributes: [:])
+                    
+                    expect { reportData.plCrashReport.exceptionInfo }.to(beNil())
+                }
+                
+                it("generated live report contains exception") {
+                    let exception = NSException(name: NSExceptionName.decimalNumberOverflowException, reason: "Test Spec")
+                    let reportData = try crashReporter.generateLiveReport(exception: exception, attributes: [:])
+                    
+                    expect { reportData.plCrashReport.exceptionInfo.exceptionName }.to(equal(NSExceptionName.decimalNumberOverflowException.rawValue))
+                    expect { reportData.plCrashReport.exceptionInfo.exceptionReason }.to(equal("Test Spec"))
                 }
             }
         }
