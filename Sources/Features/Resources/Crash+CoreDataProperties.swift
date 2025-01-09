8cbcc38f922bbfc2692b7dbf13c0399c073e9d22
melek <127785723+melekr@users.noreply.github.com>
2025-01-09 11:39:47 -0500

    Fix CoreData swift/objc attributes Interoperability (#149)
    * Explicitly expose NSManaged Objects to objc
    
    * Set xcdatamodel optional attributes & remove attachmentPaths customClassName

--- Sources/Features/Resources/Crash+CoreDataProperties.swift
+++ Sources/Features/Resources/Crash+CoreDataProperties.swift
@@ -7,10 +7,15 @@ extension Crash {
         return NSFetchRequest<Crash>(entityName: "Crash")
     }
 
+    @objc(attachmentPaths)
     @NSManaged public var attachmentPaths: [String]?
+    @objc(dateAdded)
     @NSManaged public var dateAdded: Date?
+    @objc(hashProperty)
     @NSManaged public var hashProperty: String?
+    @objc(reportData)
     @NSManaged public var reportData: Data?
+    @objc(retryCount)
     @NSManaged public var retryCount: Int64
 
 }
