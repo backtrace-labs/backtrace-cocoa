243b296176e73f45c279177ea1d4294fdf0b1d3b
melek <127785723+melekr@users.noreply.github.com>
2025-04-16 12:52:23 -0400

    Update documentation links (#156)

--- Examples/Common/Keys.m
+++ Examples/Common/Keys.m
@@ -3,24 +3,24 @@
 @implementation Keys
 
 + (NSString const *) universeName {
-    // Set your universe name here - https://support.backtrace.io/hc/en-us/articles/360040516451-What-is-a-submission-url-
+    // Set your universe name here - https://docs.saucelabs.com/error-reporting/project-setup/submission-url
     // e.g.: yourteam
     return @"yourteam";
 }
 
 + (NSString const *) backtraceUrl {
-    // Set your submission URL - https://support.backtrace.io/hc/en-us/articles/360040516451-What-is-a-submission-url-
+    // Set your submission URL - https://docs.saucelabs.com/error-reporting/project-setup/submission-url/#creating-submission-urls
     // e.g.: https://yourteam.backtrace.io
     return [NSString stringWithFormat: @"https://%@.sp.backtrace.io:6098", [Keys universeName]];
 }
 
 + (NSString const *) backtraceToken {
-    // Set your submission token - https://support.backtrace.io/hc/en-us/articles/360040105172
+    // Set your submission token - https://docs.saucelabs.com/error-reporting/project-setup/submission-url/#creating-api-tokens
     return @"token";
 }
 
 + (NSString const *) backtraceSubmissionUrl {
-    // Set your submission URL - https://support.backtrace.io/hc/en-us/articles/360040516451-What-is-a-submission-url-
+    // Set your submission URL - https://docs.saucelabs.com/error-reporting/project-setup/submission-url/#creating-submission-urls
     // e.g.: https://submit.backtrace.io/{universe}/{token}/plcrash
     return [NSString stringWithFormat: @"https://submit.backtrace.io/%@/%@/plcrash", [Keys universeName], [Keys backtraceToken]];
 }
