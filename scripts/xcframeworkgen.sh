d8f1e174d5261d4e31ef4acf514942450eea3b87
melekr <melekramki@gmail.com>
2025-09-15 20:23:09 -0400

    Trim Backtrace & PLCrashReporter slices

--- scripts/xcframeworkgen.sh
+++ scripts/xcframeworkgen.sh
@@ -33,53 +33,9 @@ xcodebuild archive \
     DEBUG_INFORMATION_FORMAT="dwarf-with-dsym" GCC_GENERATE_DEBUGGING_SYMBOLS=YES \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES SKIP_INSTALL=NO
 
-xcodebuild archive \
-    -workspace Backtrace.xcworkspace \
-    -scheme "Backtrace-iOS-lib" \
-    -destination "platform=macOS,variant=Mac Catalyst" \
-    -archivePath ${BUILD_PATH}/Backtrace-iOS-MacCatalyst-lib.xcarchive \
-    -derivedDataPath ${DERIVED_DATA_PATH} \
-    -configuration Release \
-    DEBUG_INFORMATION_FORMAT="dwarf-with-dsym" GCC_GENERATE_DEBUGGING_SYMBOLS=YES \
-    SUPPORTS_MACCATALYST=YES BUILD_LIBRARY_FOR_DISTRIBUTION=YES SKIP_INSTALL=NO    
-
-xcodebuild archive \
-    -workspace Backtrace.xcworkspace \
-    -scheme "Backtrace-macOS-lib" \
-    -destination "platform=macOS" \
-    -archivePath ${BUILD_PATH}/Backtrace-macOS-lib.xcarchive \
-    -derivedDataPath ${DERIVED_DATA_PATH} \
-    -configuration Release \
-    DEBUG_INFORMATION_FORMAT="dwarf-with-dsym" GCC_GENERATE_DEBUGGING_SYMBOLS=YES \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES SKIP_INSTALL=NO
-
-xcodebuild archive \
-    -workspace Backtrace.xcworkspace \
-    -scheme "Backtrace-tvOS-lib" \
-    -destination "generic/platform=tvOS" \
-    -archivePath ${BUILD_PATH}/Backtrace-tvOS-lib.xcarchive \
-    -derivedDataPath ${DERIVED_DATA_PATH} \
-    -configuration Release \
-    DEBUG_INFORMATION_FORMAT="dwarf-with-dsym" GCC_GENERATE_DEBUGGING_SYMBOLS=YES \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES SKIP_INSTALL=NO
-
-    xcodebuild archive \
-    -workspace Backtrace.xcworkspace \
-    -scheme "Backtrace-tvOS-lib" \
-    -destination "generic/platform=tvOS Simulator" \
-    -archivePath ${BUILD_PATH}/Backtrace-tvOS-Simulator-lib.xcarchive \
-    -derivedDataPath ${DERIVED_DATA_PATH} \
-    -configuration Release \
-    DEBUG_INFORMATION_FORMAT="dwarf-with-dsym" GCC_GENERATE_DEBUGGING_SYMBOLS=YES \
-    BUILD_LIBRARY_FOR_DISTRIBUTION=YES SKIP_INSTALL=NO
-
 xcodebuild -create-xcframework \
     -archive ${BUILD_PATH}/Backtrace-iOS-lib.xcarchive -framework Backtrace.framework \
     -archive ${BUILD_PATH}/Backtrace-iOS-Simulator-lib.xcarchive -framework Backtrace.framework \
-    -archive ${BUILD_PATH}/Backtrace-iOS-MacCatalyst-lib.xcarchive -framework Backtrace.framework \
-    -archive ${BUILD_PATH}/Backtrace-macOS-lib.xcarchive -framework Backtrace.framework \
-    -archive ${BUILD_PATH}/Backtrace-tvOS-lib.xcarchive -framework Backtrace.framework \
-    -archive ${BUILD_PATH}/Backtrace-tvOS-Simulator-lib.xcarchive -framework Backtrace.framework \
     -output ${WORKFLOW_XC_PATH}/Backtrace.xcframework
 
 rm -rf ${BUILD_PATH}
@@ -95,5 +51,11 @@ if [ ! -d "$POD_PATH" ]; then
   echo "Error: Source directory '$POD_PATH' does not exist."
   exit 1
 fi
+
+# Trim PLCrashReporter slices 
+rm -rf "$POD_PATH/CrashReporter.xcframework/ios-arm64_x86_64-maccatalyst"
+rm -rf "$POD_PATH/CrashReporter.xcframework/macos-arm64_x86_64"
+rm -rf "$POD_PATH/CrashReporter.xcframework/tvos-arm64"
+rm -rf "$POD_PATH/CrashReporter.xcframework/tvos-arm64_x86_64-simulator"
 
 cp -r "$POD_PATH" "$WORKFLOW_XC_PATH"
